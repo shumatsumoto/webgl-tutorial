9f351e1c2a6284f8710367f3c55b474a2dd286f7
Shu Matsumoto <shumatsumoto0328@gmail.com>
2023-05-03 23:30:36 +0900

    ジオメトリの種類

--- 01_three_basic/020_geometry/start/app.js
+++ 01_three_basic/020_geometry/start/app.js
@@ -16,12 +16,12 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry();
+const geometry = new THREE.TorusGeometry(10, 3, 30, 20);
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-camera.position.z = 5;
+camera.position.z = 30;
 
 let i = 0;
 function animate() {
