55924ec2db9c845c2ff8c30309c25c397f19cf69
Shu Matsumoto <shumatsumoto0328@gmail.com>
2023-05-04 23:57:17 +0900

    画像の読み込み

--- 01_three_basic/040_texture/start/app.js
+++ 01_three_basic/040_texture/start/app.js
@@ -4,36 +4,43 @@
  */
 import * as THREE from "three";
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
+init();
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+async function init() {
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
 
-// const geometry = new THREE.BoxGeometry();
-const geometry = new THREE.PlaneGeometry(20, 10);
-// const geometry = new THREE.SphereGeometry();
-// const geometry = new THREE.TorusGeometry(10, 3, 200, 20);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+  const renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
 
-camera.position.z = 30;
+  // const geometry = new THREE.BoxGeometry();
+  const geometry = new THREE.PlaneGeometry(20, 10);
+  // const geometry = new THREE.SphereGeometry();
+  // const geometry = new THREE.TorusGeometry(10, 3, 200, 20);
+  const texLoader = new THREE.TextureLoader();
+  const texture = await texLoader.loadAsync("/img/output2.jpg");
 
-let i = 0;
-function animate() {
-  requestAnimationFrame(animate);
-  // console.log(i++);
-  // cube.rotation.x = cube.rotation.x + 0.01;
-  // cube.rotation.y += 0.01;
+  const material = new THREE.MeshBasicMaterial({ map: texture });
+  const cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
 
-  renderer.render(scene, camera);
+  camera.position.z = 30;
+
+  let i = 0;
+  function animate() {
+    requestAnimationFrame(animate);
+    // console.log(i++);
+    cube.rotation.x = cube.rotation.x + 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render(scene, camera);
+  }
+
+  animate();
 }
-
-animate();
