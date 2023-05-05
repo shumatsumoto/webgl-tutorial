aefa99327d54bb65e3c6dfef15e480fa7577cbbb
Shu Matsumoto <shumatsumoto0328@gmail.com>
2023-05-05 22:42:14 +0900

    複数メッシュを配置

--- 01_three_basic/080_multiple_mesh/start/app.js
+++ 01_three_basic/080_multiple_mesh/start/app.js
@@ -17,18 +17,28 @@ async function init() {
   const renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
-  
+
   const geometry1 = new THREE.BoxGeometry(10, 10, 10);
   const geometry2 = new THREE.PlaneGeometry(20, 20);
   const geometry3 = new THREE.TorusGeometry(10, 3, 200, 20);
-  
+
   const texLoader = new THREE.TextureLoader();
   // const texture1 = await texLoader.loadAsync("/img/output1.jpg");
   // const texture2 = await texLoader.loadAsync("/img/output2.jpg");
-  const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-  
-  const mesh = new THREE.Mesh(geometry1, material);
-  scene.add(mesh);
+  const material1 = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+  const material2 = material1.clone();
+  material2.color = new THREE.Color(0x00ff00);
+  const material3 = material1.clone();
+  material3.color = new THREE.Color(0x0000ff);
+
+  const mesh1 = new THREE.Mesh(geometry1, material1);
+  mesh1.position.x -= 25;
+
+  const mesh2 = new THREE.Mesh(geometry2, material2);
+  mesh2.position.x += 25;
+
+  const mesh3 = new THREE.Mesh(geometry3, material3);
+  scene.add(mesh1, mesh2, mesh3);
 
   const axis = new THREE.AxesHelper(20);
   scene.add(axis);
@@ -36,14 +46,14 @@ async function init() {
   camera.position.z = 30;
 
   const control = new OrbitControls(camera, renderer.domElement);
-    
+
   let i = 0;
   function animate() {
     requestAnimationFrame(animate);
 
     // 回転
-    // mesh.rotation.x += 0.01;
-    // mesh.rotation.y += 0.01;
+    mesh1.rotation.x += 0.01;
+    mesh1.rotation.y += 0.01;
     // mesh.rotateX(0.01);
 
     // 平行移動
