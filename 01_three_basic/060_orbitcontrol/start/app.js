954b2489acc39b634b26ee6a7fd2308ae648e810
Shu Matsumoto <shumatsumoto0328@gmail.com>
2023-05-05 10:12:00 +0900

    OrbitControlsを使った視点の操作

--- 01_three_basic/060_orbitcontrol/start/app.js
+++ 01_three_basic/060_orbitcontrol/start/app.js
@@ -3,6 +3,10 @@
  * https://threejs.org/
  */
 import * as THREE from "three";
+import {
+  OrbitControls,
+  orbitControls,
+} from "three/examples/jsm/controls/OrbitControls";
 
 init();
 async function init() {
@@ -19,21 +23,24 @@ async function init() {
   document.body.appendChild(renderer.domElement);
 
   // const geometry = new THREE.BoxGeometry();
-  const geometry = new THREE.PlaneGeometry(32, 18);
+  // const geometry = new THREE.PlaneGeometry(32, 18);
   // const geometry = new THREE.SphereGeometry();
-  // const geometry = new THREE.TorusGeometry(10, 3, 200, 20);
+  const geometry = new THREE.TorusGeometry(10, 3, 200, 20);
   const texLoader = new THREE.TextureLoader();
   const texture2 = await texLoader.loadAsync("/img/output2.jpg");
-  const material = new THREE.MeshBasicMaterial({ map: texture2 });
-  
+  const material = new THREE.MeshBasicMaterial({ color: 0xffff });
+
   const cube = new THREE.Mesh(geometry, material);
   scene.add(cube);
+
+  const control = new OrbitControls(camera, renderer.domElement);
 
   camera.position.z = 30;
 
   let i = 0;
   function animate() {
     requestAnimationFrame(animate);
+    control.update();
     // console.log(i++);
     // cube.rotation.x = cube.rotation.x + 0.01;
     // cube.rotation.y += 0.01;
