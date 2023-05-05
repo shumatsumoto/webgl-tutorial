fe2c68e386a326a030bc7aa0288a67dcaee9679c
Shu Matsumoto <shumatsumoto0328@gmail.com>
2023-05-05 23:21:07 +0900

    ランダムにメッシュを配置１

--- 01_three_basic/090_random_mesh1/start/app.js
+++ 01_three_basic/090_random_mesh1/start/app.js
@@ -18,25 +18,36 @@ async function init() {
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
 
-  const geometry1 = new THREE.BoxGeometry(10, 10, 10);
-  const geometry2 = new THREE.PlaneGeometry(20, 20);
-  const geometry3 = new THREE.TorusGeometry(10, 3, 200, 20);
+  // const geometry1 = new THREE.BoxGeometry(10, 10, 10);
+  // const geometry2 = new THREE.PlaneGeometry(20, 20);
+  // const geometry3 = new THREE.TorusGeometry(10, 3, 200, 20);
 
-  const texLoader = new THREE.TextureLoader();
-  // const texture1 = await texLoader.loadAsync("/img/output1.jpg");
-  // const texture2 = await texLoader.loadAsync("/img/output2.jpg");
-  const material1 = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-  const material2 = material1.clone();
-  material2.color = new THREE.Color(0x00ff00);
-  const material3 = material1.clone();
-  material3.color = new THREE.Color(0x0000ff);
+  function mapRand(min, max, isInt = false) {
+    let rand = Math.random() * (max - min) + min;
+    rand = isInt ? Math.round(rand) : rand;
+    return rand;
+  }
 
-  const mesh1 = new THREE.Mesh(geometry1, material1);
-  mesh1.position.x -= 25;
-  const mesh2 = new THREE.Mesh(geometry2, material2);
-  mesh2.position.x += 25;
-  const mesh3 = new THREE.Mesh(geometry3, material3);
-  scene.add(mesh1, mesh2, mesh3);
+  function randomMesh() {
+    const geometries = [
+      new THREE.BoxGeometry(10, 10, 10),
+      new THREE.PlaneGeometry(20, 20),
+      new THREE.TorusGeometry(10, 3, 200, 20),
+    ];
+    const color = new THREE.Color(
+      mapRand(0.7, 1),
+      mapRand(0.7, 1),
+      mapRand(0.7, 1)
+    );
+    const material1 = new THREE.MeshBasicMaterial({ color });
+    const gIndex = mapRand(0, geometries.length - 1, true);
+    const mesh = new THREE.Mesh(geometries[gIndex], material1);
+    return mesh;
+  }
+
+  const mesh1 = randomMesh();
+  // mesh1.position.x -= 25;
+  scene.add(mesh1);
 
   const axis = new THREE.AxesHelper(20);
   scene.add(axis);
@@ -44,16 +55,16 @@ async function init() {
   camera.position.z = 30;
 
   const control = new OrbitControls(camera, renderer.domElement);
-    
+
   let i = 0;
   function animate() {
     requestAnimationFrame(animate);
 
     // 回転
-    mesh1.rotation.x += 0.01;
-    mesh1.rotation.y += 0.01;
-    mesh2.rotation.z += 0.01;
-    mesh3.rotation.y += 0.01;
+    // mesh1.rotation.x += 0.01;
+    // mesh1.rotation.y += 0.01;
+    // mesh2.rotation.z += 0.01;
+    // mesh3.rotation.y += 0.01;
     // mesh.rotateX(0.01);
 
     // 平行移動
