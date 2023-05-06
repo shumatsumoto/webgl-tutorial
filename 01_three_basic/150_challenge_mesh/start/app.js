29497286fbe7eb8c65bc7c8e79399c4c18a2db4e
Shu Matsumoto <shumatsumoto0328@gmail.com>
2023-05-07 07:54:12 +0900

    【チャレンジ】立方体の壁

--- 01_three_basic/150_challenge_mesh/start/app.js
+++ 01_three_basic/150_challenge_mesh/start/app.js
@@ -26,17 +26,59 @@ async function init() {
     antialias: true,
   });
   renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.shadowMap.enabled = true;
   document.body.appendChild(renderer.domElement);
 
   // ライト
+  const amLight = new THREE.AmbientLight(0x3f3f46);
+  scene.add(amLight);
+
+  const pLight = new THREE.PointLight(0xffffff, 1, 200);
+  pLight.position.set(-26, 7, 100);
+  scene.add(pLight);
+  pLight.shadow.mapSize.width = 1024;
+  pLight.shadow.mapSize.height = 1024;
+  pLight.castShadow = true;
+
+  const dLight = new THREE.DirectionalLight(0xaabbff, 0.3);
+  dLight.position.set(0, 0, 1);
+  scene.add(dLight);
 
   // メッシュ
+  const X_NUM = 10;
+  const Y_NUM = 6;
+  const SCALE = 10;
+  const COLORS = {
+    MAIN: "#f3f4f6",
+    SUB: "#60a5fa",
+  };
+  const boxGeo = new THREE.BoxGeometry(SCALE, SCALE, SCALE);
+  const mainMate = new THREE.MeshLambertMaterial({ color: COLORS.MAIN });
+  const subMate = new THREE.MeshLambertMaterial({ color: COLORS.SUB });
 
+  const boxes = [];
+  for (let y = 0; y <= Y_NUM; y++) {
+    for (let x = 0; x <= X_NUM; x++) {
+      const material = Math.random() < 0.2 ? subMate : mainMate;
+      const box = new THREE.Mesh(boxGeo, material);
+      box.position.x = x * SCALE - (X_NUM * SCALE) / 2;
+      box.position.y = y * SCALE - (Y_NUM * SCALE) / 2;
+      box.position.z = mapRand(-10, 10);
+      box.scale.set(0.98, 0.98, 0.98);
+      box.castShadow = true;
+      box.receiveShadow = true;
+      boxes.push(box);
+    }
+  }
+  scene.add(...boxes);
+
+  const axis = new THREE.AxesHelper(250);
+  scene.add(axis);
   const control = new OrbitControls(camera, renderer.domElement);
 
   function animate() {
     requestAnimationFrame(animate);
-    
+
     control.update();
 
     renderer.render(scene, camera);
