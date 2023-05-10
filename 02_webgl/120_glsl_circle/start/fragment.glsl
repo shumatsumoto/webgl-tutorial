47d1dd523d3ec8d70dc3bc6d2f59a1c35d8cff8b
Shu Matsumoto <shumatsumoto0328@gmail.com>
2023-05-11 00:11:36 +0900

    円の描写と座標の正規化

--- 02_webgl/120_glsl_circle/start/fragment.glsl
+++ 02_webgl/120_glsl_circle/start/fragment.glsl
@@ -6,7 +6,9 @@ varying vec2 vVertexPosition;
 void main() {
     // vVertexPosition = (-1, -1) => (1, 1)
     // colorTmp = (0, 0) => (1, 1)
-    vec2 colorTmp = vVertexPosition / 2.0 + 0.5;
-    vec4 color = vec4(colorTmp, 0.0, 1.0);
+    vec2 p = vVertexPosition * vec2(2.0, 1.0);
+    float len = length(p) * 2.0;
+    float circle = 1. - smoothstep(.98, 1.0, len);
+    vec4 color = vec4(circle, 0.0, 0.0, 1.0);
     gl_FragColor = color;
 }
