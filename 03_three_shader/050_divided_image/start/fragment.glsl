5a487e4c1be706e3d77a4183c08b15fa482c448d
Shu Matsumoto <shumatsumoto0328@gmail.com>
2023-05-14 11:49:41 +0900

    画像の繰り返し表示

--- 03_three_shader/050_divided_image/start/fragment.glsl
+++ 03_three_shader/050_divided_image/start/fragment.glsl
@@ -7,10 +7,12 @@ void main() {
     float time = 20.05;
     const float AMP = 0.5;
     vec4 color = vec4(0.0, 0.0, 0.0, 1.0);
-    color.r = texture(uTex, vUv + vec2(AMP * sin(time), 0.0)).r;
-    color.g = texture(uTex, vUv + vec2(0., AMP * sin(time))).g;
-    color.b = texture(uTex, vUv + vec2(0.0, 0.0)).b;
-    
-    gl_FragColor = color;
+    color = texture(uTex, fract(vUv * 5.));
+    vec4 color2 = vec4(1.0);
+    color2.rgb = color.gbr;
+    // color.g = texture(uTex, vUv + vec2(0., AMP * sin(time))).g;
+    // color.b = texture(uTex, vUv + vec2(0.0, 0.0)).b;
+
+    gl_FragColor = color2;
     // gl_FragColor = texColor;
 }
