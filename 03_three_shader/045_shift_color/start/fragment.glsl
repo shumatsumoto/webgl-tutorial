2b892197e43e0efbe0fed23b557b5f05d872fe7a
Shu Matsumoto <shumatsumoto0328@gmail.com>
2023-05-14 11:39:56 +0900

    カラーシフトとテクスチャラッピング

--- 03_three_shader/045_shift_color/start/fragment.glsl
+++ 03_three_shader/045_shift_color/start/fragment.glsl
@@ -4,20 +4,12 @@ uniform float uTick;
 
 void main() {
     float time = uTick * 0.01;
+    const float AMP = 0.1;
     vec4 color = vec4(0.0, 0.0, 0.0, 1.0);
-    vec4 texColor = texture(uTex, vUv);
+    color.r = texture(uTex, vUv + vec2(AMP * sin(time), 0.0)).r;
+    color.g = texture(uTex, vUv + vec2(0., AMP * sin(time))).g;
+    color.b = texture(uTex, vUv + vec2(0., 0.0)).b;
 
-    // 一行で色の入れ替え
-    // color.rgb = texColor.gbr;
-    
-    // 時間で色の変化
-    // color.r = texColor.g * sin(time);
-    // color.g = texColor.b * cos(time);
-    // color.b = texColor.r;
-
-    // 色を反転
-    color.rgb = 1.0 - texColor.rgb;
-    
     gl_FragColor = color;
     // gl_FragColor = texColor;
 }
