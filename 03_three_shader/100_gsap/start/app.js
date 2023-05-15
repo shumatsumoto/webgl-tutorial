07f4a1e5eab36d285bc465ecb15487509dad4b65
Shu Matsumoto <shumatsumoto0328@gmail.com>
2023-05-15 10:47:40 +0900

    GSAPの基本的な使い方について学ぼう

--- 03_three_shader/100_gsap/start/app.js
+++ 03_three_shader/100_gsap/start/app.js
@@ -1,7 +1,7 @@
 /**
  * GSAP
  * https://greensock.com/
- * 
+ *
  * GSAP Eases
  * https://greensock.com/docs/v3/Eases
  */
@@ -9,5 +9,30 @@ import { gsap } from "gsap";
 
 init();
 async function init() {
-  
+  gsap.to(".box", {
+    y: 200,
+    borderRadius: "50%",
+    scale: 2,
+    backgroundColor: "#ff0000",
+    rotate: 360,
+    duration: 2,
+    ease: "power3.inOut",
+    delay: 1,
+    repeat: -1,
+    yoyo: true,
+  });
+
+  const obj = { value: 0 };
+  gsap.to(obj, {
+    value: 1,
+    duration: 2,
+    delay: 1,
+    ease: "back.inOut(1.7)",
+    onUpdate() {
+      console.log(obj.value);
+    },
+    onComplete() {
+      console.log("Animation End!");
+    },
+  });
 }
