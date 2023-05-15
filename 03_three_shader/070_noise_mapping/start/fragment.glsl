e58969e55469b5fee06d8a77ea56e9236c6ea7a5
Shu Matsumoto <shumatsumoto0328@gmail.com>
2023-05-15 09:29:38 +0900

    ノイズの使い方を見てみよう

--- 03_three_shader/070_noise_mapping/start/fragment.glsl
+++ 03_three_shader/070_noise_mapping/start/fragment.glsl
@@ -12,25 +12,25 @@ void main() {
   float n = noise2(vUv);
 
   // より細かく
-  // n = noise2(vUv * 10.);
+  n = noise2(vUv * 10.);
 
   // 時間が経つにつれて細かく
-  // n = noise2(vUv * uTick * 0.1);
+  n = noise2(vUv * uTick * 0.1);
 
   // ノイズを移動
-  // n = noise2(vUv * 10. - uTick * 0.01);
+  n = noise2(vUv * 10. - uTick * 0.01);
 
   // x方向に移動
-  // n = noise2(vec2(vUv.x * 10. - uTick * 0.01, vUv.y));
+  n = noise2(vec2(vUv.x * 10. - uTick * 0.01, vUv.y * 10.));
 
   // 波のように揺れる
-  // n = noise2(vec2(vUv.x * 100. - sin(vUv.y + uTick / 100.), vUv.y * 10. - sin(vUv.x + uTick / 100.)));
+  n = noise2(vec2(vUv.x * 100. - sin(vUv.y + uTick / 100.), vUv.y * 10. - sin(vUv.x + uTick / 100.)));
 
   // 模様を変化させる
-  // n = noise3(vec3(vUv * 10., uTick * 0.01));
+  n = noise3(vec3(vUv * 10., uTick * 0.01));
 
   // ヨコシマのノイズ
-  //   n = noise3(vec3(vUv.x * 10., vUv.y * 50., uTick * 0.01));
+  n = noise3(vec3(vUv.x * 10., vUv.y * 50., uTick * 0.01));
 
   gl_FragColor = vec4(n, 0., 0., 1.);
 }
