48a1f80869b1a5d179328e82768852b2826d7fed
Shu Matsumoto <shumatsumoto0328@gmail.com>
2023-05-16 08:53:03 +0900

    スティッキーな画像メッシュを作成してみよう２

--- 04_three_advance/030_sticky_plane2/start/vertex.glsl
+++ 04_three_advance/030_sticky_plane2/start/vertex.glsl
@@ -1,11 +1,17 @@
 precision mediump float;
+#pragma glslify: easeBack = require(glsl-easings/back-in-out)
 
 varying vec2 vUv;
 varying float vDelay;
 attribute float aDelay;
 
+uniform float uProgress;
+
 void main() {
     vUv = uv;
     vDelay = aDelay;
-    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    vec3 pos = position;
+    float progress = easeBack(clamp(uProgress * 1.4 - aDelay * .3, 0., 1.));
+    pos.z += progress * 250.;
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
 }
